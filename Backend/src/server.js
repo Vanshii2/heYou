80bdef347727d4bd9bbb82796c3f9aa785912af2
Vanshita Tripathi <vanshupro99@gmail.com>
2025-12-17 13:18:34 +0530

    ready for deployement

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -2,15 +2,26 @@ import express from 'express'
 import dotenv from 'dotenv'
 import authRoutes from './routes/auth.route.js';
 import messageRoutes from './routes/message.route.js'
+import path from 'path';
 
 dotenv.config();
 
 const PORT = process.env.PORT || 3000
 
 const app = express();
+const __dirname= path.resolve();
 
 app.use("/api/auth", authRoutes)
 app.use("/api/messages", messageRoutes)
+
+//  make ready deployement
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname,"../Frontend/dist")));
+
+    app.get("*",(_,res)=>{
+        res.sendFile(path.join(__dirname, "../frontend","dist","index.html"))
+    })
+}
 
 
 app.listen(PORT,()=>{
